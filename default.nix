8a6c32503c91250c26a5f32c1e21591771d776aa
Lucas Savva <lucas@m1cr0man.com>
2023-01-18 19:21:34 +0000

    Add flake.nix, convert default.nix

--- default.nix
+++ default.nix
@@ -1,20 +1,8 @@
-{ pkgs ? import ./nixpkgs.nix {} }:
-
 let
-  nixpkgsPath = pkgs.path;
-  fromPkgs = path: pkgs.path + "/${path}";
-  evalConfig = import (fromPkgs "nixos/lib/eval-config.nix");
-  buildConfig = { configuration ? {} }:
-    evalConfig {
-      specialArgs = { inherit nixpkgsPath; };
-      modules= [
-        ./modules
-        configuration
-      ];
-    }
-  ;
-  eval = buildConfig { };
-in
-{
-  inherit (eval) pkgs;
-}
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  compat = builtins.fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  flake = (import compat { src = ./.; }).defaultNix;
+in flake.legacyPackages.x86_64-linux // flake.packages.x86_64-linux
