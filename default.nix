07d2fc3af9e6de664b3a5087b65e4c200601e5c9
Lucas Savva <lucas@m1cr0man.com>
2023-01-18 19:21:34 +0000

    Add flake.nix, convert default.nix

--- default.nix
+++ default.nix
@@ -1,39 +1,8 @@
-{ pkgs ? import ./nixpkgs.nix {} }:
-
 let
-  nixpkgsPath = pkgs.path;
-  fromPkgs = path: pkgs.path + "/${path}";
-  evalConfig = import (fromPkgs "nixos/lib/eval-config.nix");
-  buildConfig = { configuration ? {} }:
-    evalConfig {
-      specialArgs = {
-        inherit nixpkgsPath;
-      };
-      modules= [
-        ./modules
-        configuration
-        ({ lib, config, ... }: {
-          jovian.devices.steamdeck.enable = true;
-
-          # Override config in installation-cd-graphical-base.nix
-          hardware.pulseaudio.enable = lib.mkIf
-            (config.jovian.devices.steamdeck.enableSoundSupport && config.services.pipewire.enable)
-            (lib.mkForce false);
-        })
-      ];
-    }
-  ;
-  eval = buildConfig { };
-in
-{
-  isoMinimal = (buildConfig {
-    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-minimal.nix");
-  }).config.system.build.isoImage;
-  isoGnome = (buildConfig {
-    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix");
-  }).config.system.build.isoImage;
-  isoPlasma = (buildConfig {
-    configuration = (fromPkgs "nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix");
-  }).config.system.build.isoImage;
-  inherit (eval) pkgs;
-}
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  compat = builtins.fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash;
+  };
+  flake = (import compat { src = ./.; }).defaultNix;
+in flake.legacyPackages.x86_64-linux // flake.packages.x86_64-linux
